a0cfb3d0227ed7f2170497af057c2dbd8ace061a
nahom2311 <nahomsahile3@gmail.com>
2025-06-28 19:39:26 -0600

    Numeric Types

--- basics.py
+++ basics.py
@@ -1,1 +1,25 @@
-print("hello world")
+# Day 1 
+
+##Types of python files 
+
+# 1 Numeric Types 
+
+#1a int; Just regular integer numbers (no decimal numbers)
+
+x = -5
+
+print(type(x))
+
+#1b float; Decimal numbers 
+
+y = 3.22 
+
+print(type(y))
+
+#1c complex; numbers and digits together
+
+z = 1j 
+
+print(type(z))
+
+# 2 
